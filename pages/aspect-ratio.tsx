1b563b03416260ba07cb92e56705e77600df511b
dlford <dan-github2@dlford.co>
2022-09-06 08:04:12 -0700

    progress on aspect ratio

--- pages/aspect-ratio.tsx
+++ pages/aspect-ratio.tsx
@@ -1,7 +1,13 @@
 import AspectRatioIcon from '@mui/icons-material/AspectRatio';
 import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import OutlinedInput from '@mui/material/OutlinedInput';
 import Paper from '@mui/material/Paper';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { Theme, useTheme } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -64,13 +70,78 @@ export default function AspectRatioPage() {
   const [newWidth, setNewWidth] = React.useState<number | void>(1440);
   const [newHeight, setNewHeight] = React.useState<number | void>(810);
 
-  const layoutWidths = [640, 1024, 1440, 1920];
-  const layoutCount = 4;
+  /* widths selector */
+
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
+  const names = [
+    '320',
+    '480',
+    '600',
+    '640',
+    '768',
+    '800',
+    '1000',
+    '1024',
+    '1200',
+    '1280',
+    '1366',
+    '1440',
+    '1600',
+    '1920',
+    '2560',
+    '3840',
+  ];
+
+  function getStyles(
+    name: string,
+    personName: readonly string[],
+    theme: Theme,
+  ) {
+    return {
+      fontWeight:
+        personName.indexOf(name) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightMedium,
+    };
+  }
+
+  const theme = useTheme();
+  const [layoutWidths, setLayoutWidths] = React.useState<string[]>([
+    '640',
+    '1024',
+    '1440',
+    '1920',
+  ]);
+
+  const handleLayoutSizeChange = (
+    event: SelectChangeEvent<typeof layoutWidths>,
+  ) => {
+    const {
+      target: { value },
+    } = event;
+    const numbers =
+      typeof value === 'string' ? value.split(',') : value;
+    setLayoutWidths(
+      numbers.sort((a, b) => parseInt(a, 10) - parseInt(b, 10)),
+    );
+  };
+
+  /* widths selector */
+
   const getLayoutData = useLayoutData({
     width,
     height,
-    layoutWidths,
-    layoutCount,
+    layoutWidths: layoutWidths.map((str) => parseInt(str, 10)),
   });
 
   const layoutData = getLayoutData();
@@ -239,9 +310,50 @@ export default function AspectRatioPage() {
       >
         Layouts
       </Typography>
+      <div className='widths'>
+        <FormControl sx={{ m: 1, width: 240 }}>
+          <InputLabel id='demo-multiple-chip-label'>
+            Screen Widths
+          </InputLabel>
+          <Select
+            labelId='demo-multiple-chip-label'
+            id='demo-multiple-chip'
+            multiple
+            value={layoutWidths}
+            onChange={handleLayoutSizeChange}
+            input={
+              <OutlinedInput
+                id='select-multiple-chip'
+                label='Screen Widths'
+              />
+            }
+            renderValue={(selected) => (
+              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                {selected.map((value) => (
+                  <Chip
+                    key={value}
+                    label={value}
+                  />
+                ))}
+              </Box>
+            )}
+            MenuProps={MenuProps}
+          >
+            {names.map((name) => (
+              <MenuItem
+                key={name}
+                value={name}
+                style={getStyles(name, layoutWidths, theme)}
+              >
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
       <TableContainer
         component={Paper}
-        sx={{ mb: 3 }}
+        sx={{ mb: 3, maxWidth: 1000 }}
       >
         <Table
           sx={{ minWidth: 650 }}
