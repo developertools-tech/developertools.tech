51590c36aa69ee64a5dc39a91fa9e1acc16dcfc1
dlford <dan-github2@dlford.co>
2022-09-05 18:18:50 -0700

    progress on aspect ratio

--- pages/aspect-ratio.tsx
+++ pages/aspect-ratio.tsx
@@ -9,8 +9,9 @@ import * as React from 'react';
 import Heading from '../components/Heading';
 import Layout from '../components/Layout';
 
-const previewWidth = 226;
+const previewMaxSize = 226;
 
+// BUG: Low numbers like 1 or 3 give wrong results
 function getPreviewSize({
   width,
   height,
@@ -21,33 +22,79 @@ function getPreviewSize({
   const aspectRatio = width / height;
   if (width > height) {
     return {
-      width: previewWidth,
-      height: previewWidth / aspectRatio,
+      width: previewMaxSize,
+      height: previewMaxSize / aspectRatio,
     };
   }
   return {
-    width: previewWidth * aspectRatio,
-    height: previewWidth,
+    width: previewMaxSize * aspectRatio,
+    height: previewMaxSize,
   };
 }
 
-function getAspectRatio({
+function getDisplayRatio({
   width,
   height,
 }: {
-  width: number;
-  height: number;
+  width: number | void;
+  height: number | void;
 }): string {
+  if (!width || !height) {
+    return '...';
+  }
   const gcd = (a: number, b: number): number => (b ? gcd(b, a % b) : a);
   const divisor = gcd(width, height);
   return `${width / divisor}:${height / divisor}`;
 }
 
 export default function AspectRatioPage() {
-  const [width, setWidth] = React.useState(1920);
-  const [height, setHeight] = React.useState(1080);
+  const [width, setWidth] = React.useState<number | void>(1920);
+  const [height, setHeight] = React.useState<number | void>(1080);
+  const [newWidth, setNewWidth] = React.useState<number | void>(1440);
+  const [newHeight, setNewHeight] = React.useState<number | void>(810);
 
-  const previewSize = getPreviewSize({ width, height });
+  const hasWidthHeight = !!width && !!height;
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value, name } = event.currentTarget;
+    const number = Number(value.replace(/\D/g, ''));
+
+    if (name === 'width') {
+      setWidth(number);
+      if (newHeight && height) {
+        const aspectRatio = number / height;
+        setNewWidth(Math.round(newHeight * aspectRatio));
+      }
+    }
+
+    if (name === 'height') {
+      setHeight(number);
+      if (newWidth && width) {
+        const aspectRatio = width / number;
+        setNewHeight(Math.round(newWidth / aspectRatio));
+      }
+    }
+
+    if (name === 'newWidth') {
+      setNewWidth(number);
+      if (width && height) {
+        const aspectRatio = width / height;
+        setNewHeight(Math.round(number / aspectRatio));
+      }
+    }
+
+    if (name === 'newHeight') {
+      setNewHeight(number);
+      if (width && height) {
+        const aspectRatio = width / height;
+        setNewWidth(Math.round(number * aspectRatio));
+      }
+    }
+  }
+
+  const previewSize = hasWidthHeight
+    ? getPreviewSize({ width, height })
+    : { width: undefined, height: undefined };
 
   return (
     <Layout title='Aspect Ratio'>
@@ -58,35 +105,49 @@ export default function AspectRatioPage() {
         justifyContent='center'
         alignItems='center'
         gap={2}
+        columnGap={8}
       >
         <Box
-          width={previewWidth}
-          height={previewWidth}
           display='flex'
           flexDirection='column'
-          gap={2}
-          justifyContent='center'
           alignItems='center'
+          justifyContent='start'
+          gap={2}
+          pb={2}
         >
-          <Chip
-            icon={<AspectRatioIcon />}
-            label={getAspectRatio({ width, height })}
-            sx={{ p: 0.5 }}
-          />
-          <Paper
-            elevation={3}
-            sx={{
-              width: previewSize.width,
-              height: previewSize.height,
-              overflow: 'auto',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
+          <Box
+            width={previewMaxSize}
+            height={previewMaxSize}
+            display='flex'
+            flexDirection='column'
+            gap={2}
+            justifyContent='center'
+            alignItems='center'
           >
-            <ImageIcon />
-          </Paper>
+            {hasWidthHeight && (
+              <Paper
+                elevation={3}
+                sx={{
+                  width: previewSize.width,
+                  height: previewSize.height,
+                  overflow: 'auto',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <ImageIcon />
+              </Paper>
+            )}
+          </Box>
+          {hasWidthHeight && (
+            <Chip
+              icon={<AspectRatioIcon />}
+              label={getDisplayRatio({ width, height })}
+              sx={{ p: 0.5 }}
+            />
+          )}
         </Box>
         <form>
           <Box
@@ -106,20 +167,16 @@ export default function AspectRatioPage() {
               <TextField
                 label='Source Width'
                 variant='outlined'
-                type='number'
-                value={width}
-                onChange={(event) =>
-                  setWidth(Number(event.target.value))
-                }
+                value={width || ''}
+                name='width'
+                onChange={handleChange}
               />
               <TextField
                 label='Source Height'
                 variant='outlined'
-                type='number'
-                value={height}
-                onChange={(event) =>
-                  setHeight(Number(event.target.value))
-                }
+                value={height || ''}
+                name='height'
+                onChange={handleChange}
               />
             </Box>
             <Box
@@ -132,12 +189,16 @@ export default function AspectRatioPage() {
               <TextField
                 label='Target Width'
                 variant='outlined'
-                type='number'
+                value={newWidth || ''}
+                name='newWidth'
+                onChange={handleChange}
               />
               <TextField
                 label='Target Height'
                 variant='outlined'
-                type='number'
+                value={newHeight || ''}
+                name='newHeight'
+                onChange={handleChange}
               />
             </Box>
           </Box>
