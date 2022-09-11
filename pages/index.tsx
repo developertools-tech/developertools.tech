a1048e6cd88249ff1235970f13d39e7ad2db7bc2
dlford <dan-github2@dlford.co>
2022-09-10 22:52:56 -0700

    update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,14 +13,37 @@ export default function IndexPage() {
         <Typography paragraph>
           Choose a tool from the menu to get started!
         </Typography>
-        <Typography paragraph>
+        <Typography
+          paragraph
+          mt={8}
+        >
           Don&apos;t see the tool you need?{' '}
           <a
             href='https://github.com/developertools-tech/developertools.tech/issues/new?assignees=&labels=feature+request%2Ctriage&template=FEATURE_REQUEST.yml&title=%5BReq%5D%3A+'
             target='_blank'
             rel='noopener noreferrer'
           >
-            Request it here
+            request it here
+          </a>
+        </Typography>
+        <Typography paragraph>
+          Found a bug?{' '}
+          <a
+            href='https://github.com/developertools-tech/developertools.tech/issues/new?assignees=&labels=bug%2Ctriage&template=BUG.yml&title=%5BBug%5D%3A+'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            report it here
+          </a>
+        </Typography>
+        <Typography paragraph>
+          Want to help?{' '}
+          <a
+            href='https://github.com/developertools-tech/developertools.tech'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            contribute on GitHub
           </a>
         </Typography>
       </Box>
