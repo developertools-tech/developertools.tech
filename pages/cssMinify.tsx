736490082b756e650f380be1d8ad7589de1d0b77
Dustin Lammiman <dustin@nosecreekweb.ca>
2022-10-05 13:59:31 -0600

    Add cssMinify Page

---
+++ pages/cssMinify.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Heading from '../components/Heading';
+import Layout from '../components/Layout';
+
+export default function CssMinifyPage() {
+  return (
+    <Layout>
+      <Heading>Minify CSS</Heading>
+    </Layout>
+  );
+}
