a11d41f84517ce79d97583a99b16da0e77866395
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-10-11 16:50:28 +0200

    progress: made handlers

--- pages/colors.tsx
+++ pages/colors.tsx
@@ -2,42 +2,77 @@ import { Container, TextField, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import * as convert from 'colors-convert';
 import { HEX, HSL, RGB } from 'colors-convert/dist/cjs/lib/types/types';
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { SketchPicker } from 'react-color';
 
 import Heading from '../components/Heading';
 import Layout from '../components/Layout';
 
 export default function Colors() {
-  const [colorPickerColor, setColorPickerColor] = useState<string>(
-    convert.randomHex(),
-  );
+  const [colorPickerColor, setColorPickerColor] =
+    useState<HEX>('#000000');
   const [hexColor, setHexColor] = useState<HEX>(colorPickerColor);
-  const [hslColor, setHslColor] = useState<HSL>({
-    h: 0,
-    s: 100,
-    l: 50,
-  });
-  const [rgbColor, setRGBColor] = useState<RGB>({ r: 255, g: 0, b: 0 });
+  const [hslColor, setHslColor] = useState<HSL>({ h: 0, s: 0, l: 0 });
+  const [rgbColor, setRGBColor] = useState<RGB>({ r: 0, g: 0, b: 0 });
   const [err, setErr] = useState<string>();
 
-  const updateColors = (color: string) => {
+  function HandleHexChange(hex: HEX) {
+    setHexColor(hex);
+    setColorPickerColor(hex);
+    setHslColor(convert.hexToHsl(hex));
+    setRGBColor(convert.hexToRgb(hex));
+  }
+
+  function HandleRGBChange(value: string) {
+    const valArr = value.split(',');
+    const rgb = {
+      r: parseInt(valArr[0], 10),
+      g: parseInt(valArr[1], 10),
+      b: parseInt(valArr[2], 10),
+    };
+    setRGBColor(rgb);
+    setHexColor(convert.rgbToHex(rgb));
+    setHslColor(convert.rgbToHsl(rgb));
+  }
+
+  function HandleHSLChange(value: string) {
+    const valArr = value.split(',');
+    const hsl = {
+      h: parseInt(valArr[0], 10),
+      s: parseInt(valArr[1], 10),
+      l: parseInt(valArr[2], 10),
+    };
+    setHslColor(hsl);
+    setRGBColor(convert.hslToRgb(hsl));
+    setHexColor(convert.hslToHex(hsl));
+  }
+  const updateColors = (
+    eventData:
+      | ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+      | { target: { name: string; value: string } },
+  ) => {
     try {
       setErr('');
-      setColorPickerColor(color);
-      setHexColor(color);
-      setHslColor(convert.hexToHsl(color));
-      setRGBColor(convert.hexToRgb(color));
+      const { name, value } = eventData.target;
+      switch (name) {
+        case 'hex':
+          HandleHexChange(value);
+          break;
+        case 'rgb':
+          HandleRGBChange(value);
+          break;
+        case 'hsl':
+          HandleHSLChange(value);
+          break;
+        default:
+          break;
+      }
     } catch (e: unknown) {
       if (e instanceof Error) {
         setErr(e.message);
       }
     }
   };
-
-  useEffect(() => {
-    updateColors(hexColor);
-  }, []);
 
   return (
     <Layout>
@@ -51,7 +86,9 @@ export default function Colors() {
             disableAlpha
             color={colorPickerColor}
             onChange={(color) => {
-              updateColors(color.hex);
+              updateColors({
+                target: { name: 'hex', value: color.hex },
+              });
             }}
           />
           <Box
@@ -64,47 +101,33 @@ export default function Colors() {
           >
             <TextField
               label='HEX'
-              value={hexColor}
+              name='hex'
+              defaultValue={hexColor}
               InputLabelProps={{
                 shrink: true,
               }}
               onChange={(e) => {
-                updateColors(e.target.value);
+                updateColors(e);
               }}
             />
             <TextField
               label='HSL'
+              name='hsl'
+              defaultValue={`${hslColor?.h}, ${hslColor?.s}, ${hslColor?.l}`}
               value={`${hslColor?.h}, ${hslColor?.s}, ${hslColor?.l}`}
               InputLabelProps={{
                 shrink: true,
               }}
               onChange={(e) => {
-                const value = e.target.value.split(',');
-                const hsl = {
-                  h: parseInt(value[0], 2),
-                  s: parseInt(value[1], 2),
-                  l: parseInt(value[2], 2),
-                };
-                updateColors(convert.hslToHex(hsl));
+                updateColors(e);
               }}
             />
             <TextField
               label='RGB'
-              value={`${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b}`}
+              name='rgb'
+              defaultValue={`${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b}`}
               onChange={(e) => {
-                try {
-                  const value = e.target.value.split(',');
-                  const rgb = {
-                    r: parseInt(value[0], 10),
-                    g: parseInt(value[1], 10),
-                    b: parseInt(value[2], 10),
-                  };
-                  updateColors(convert.rgbToHex(rgb));
-                } catch (error: unknown) {
-                  if (e instanceof Error) {
-                    setErr(e.message);
-                  }
-                }
+                updateColors(e);
               }}
               InputLabelProps={{
                 shrink: true,
