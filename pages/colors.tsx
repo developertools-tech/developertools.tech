c39ede287d013824142155b6a4336de0c60f6d33
dlford <dan-github2@dlford.co>
2022-10-15 12:56:38 -0700

    fix: handle user input gracefully

--- pages/colors.tsx
+++ pages/colors.tsx
@@ -2,47 +2,99 @@ import { Container, TextField, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import * as convert from 'colors-convert';
 import { HEX, HSL, RGB } from 'colors-convert/dist/cjs/lib/types/types';
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent } from 'react';
 import { HexColorPicker } from 'react-colorful';
 
 import Heading from '../components/Heading';
 import Layout from '../components/Layout';
+import useLocalState from '../hooks/useLocalState';
+
+function serializeColor(value: HEX | HSL | RGB): string {
+  if (typeof value === 'object') {
+    return Object.values(value).join(', ');
+  }
+  return value;
+}
+
+function stringToNumArray(value: string): number[] | string {
+  const valArr = value.split(',');
+  if (
+    valArr?.length >= 3 &&
+    valArr.every((v) => !Number.isNaN(Number(v) || NaN))
+  ) {
+    return value.split(',').map((v) => parseInt(v, 10));
+  }
+
+  return value;
+}
 
 export default function Colors() {
-  const [hexColor, setHexColor] = useState<HEX>('#000000');
-  const [hslColor, setHslColor] = useState<HSL>({ h: 0, s: 0, l: 0 });
-  const [rgbColor, setRGBColor] = useState<RGB>({ r: 0, g: 0, b: 0 });
-  const [err, setErr] = useState<string>();
+  const [pickerColor, setPickerColor] = useLocalState<string>({
+    key: 'colorPicker_pickerColor',
+    defaultValue: '#000000',
+  });
+  const [hexColor, setHexColor] = useLocalState<string>({
+    key: 'colorPicker_hexColor',
+    defaultValue: '#000000',
+  });
+  const [hslColor, setHslColor] = useLocalState<string>({
+    key: 'colorPicker_hslColor',
+    defaultValue: '0, 0, 0',
+  });
+  const [rgbColor, setRGBColor] = useLocalState<string>({
+    key: 'colorPicker_rgbColor',
+    defaultValue: '0, 0, 0',
+  });
+  const [err, setErr] = useLocalState<string>({
+    key: 'colorPicker_err',
+    defaultValue: '',
+  });
 
-  function HandleHexChange(hex: HEX) {
+  function HandleHexChange(hex: string) {
     setHexColor(hex);
-    setHslColor(convert.hexToHsl(hex));
-    setRGBColor(convert.hexToRgb(hex));
+    if (/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(hex)) {
+      setHslColor(serializeColor(convert.hexToHsl(hex)));
+      setRGBColor(serializeColor(convert.hexToRgb(hex)));
+      setPickerColor(hex);
+    }
   }
 
   function HandleRGBChange(value: string) {
-    const valArr = value.split(',');
-    const rgb = {
-      r: parseInt(valArr[0], 10),
-      g: parseInt(valArr[1], 10),
-      b: parseInt(valArr[2], 10),
-    };
-    setRGBColor(rgb);
-    setHexColor(convert.rgbToHex(rgb));
-    setHslColor(convert.rgbToHsl(rgb));
+    const valArr = stringToNumArray(value);
+    if (Array.isArray(valArr)) {
+      const rgb = {
+        r: valArr[0] <= 255 ? valArr[0] : 255,
+        g: valArr[1] <= 255 ? valArr[1] : 255,
+        b: valArr[2] <= 255 ? valArr[2] : 255,
+      };
+      const hex = convert.rgbToHex(rgb);
+      setHexColor(serializeColor(hex));
+      setPickerColor(hex);
+      setRGBColor(serializeColor(rgb));
+      setHslColor(serializeColor(convert.rgbToHsl(rgb)));
+    } else {
+      setRGBColor(value);
+    }
   }
 
   function HandleHSLChange(value: string) {
-    const valArr = value.split(',');
-    const hsl = {
-      h: parseInt(valArr[0], 10),
-      s: parseInt(valArr[1], 10),
-      l: parseInt(valArr[2], 10),
-    };
-    setHslColor(hsl);
-    setRGBColor(convert.hslToRgb(hsl));
-    setHexColor(convert.hslToHex(hsl));
+    const valArr = stringToNumArray(value);
+    if (Array.isArray(valArr)) {
+      const hsl = {
+        h: valArr[0] <= 100 ? valArr[0] : 100,
+        s: valArr[1] <= 100 ? valArr[1] : 100,
+        l: valArr[2] <= 100 ? valArr[2] : 100,
+      };
+      const hex = convert.hslToHex(hsl);
+      setPickerColor(hex);
+      setHexColor(serializeColor(hex));
+      setHslColor(serializeColor(hsl));
+      setRGBColor(serializeColor(convert.hslToRgb(hsl)));
+    } else {
+      setHslColor(value);
+    }
   }
+
   const updateColors = (
     eventData:
       | ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -80,7 +132,7 @@ export default function Colors() {
           gap='2rem'
         >
           <HexColorPicker
-            color={hexColor}
+            color={pickerColor}
             onChange={(color) => {
               updateColors({
                 target: { name: 'hex', value: color },
@@ -102,28 +154,22 @@ export default function Colors() {
               InputLabelProps={{
                 shrink: true,
               }}
-              onChange={(e) => {
-                updateColors(e);
-              }}
+              onChange={updateColors}
             />
             <TextField
               label='HSL'
               name='hsl'
-              value={`${hslColor?.h}, ${hslColor?.s}, ${hslColor?.l}`}
+              value={hslColor}
               InputLabelProps={{
                 shrink: true,
               }}
-              onChange={(e) => {
-                updateColors(e);
-              }}
+              onChange={updateColors}
             />
             <TextField
               label='RGB'
               name='rgb'
-              value={`${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b}`}
-              onChange={(e) => {
-                updateColors(e);
-              }}
+              value={rgbColor}
+              onChange={updateColors}
               InputLabelProps={{
                 shrink: true,
               }}
