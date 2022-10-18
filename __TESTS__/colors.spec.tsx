af987056443c137a39438cd188007a1a5e9974f9
dlford <dan-github2@dlford.co>
2022-10-17 17:59:26 -0700

    fix tests and adjust UI

--- __TESTS__/colors.spec.tsx
+++ __TESTS__/colors.spec.tsx
@@ -16,10 +16,12 @@ describe('Colors', () => {
     const rgb = screen.getByLabelText('RGB');
 
     await user.clear(hex);
-    await user.type(hex, '#000000');
-    expect(hsl.innerHTML).toBe('0, 0, 0');
-    expect(rgb.innerHTML).toBe('0, 0, 0');
+    await user.type(hex, '232c34');
+    expect(hex).toHaveValue('232c34');
+    expect(hsl).toHaveValue('212, 17, 17');
+    expect(rgb).toHaveValue('36, 43, 51');
   });
+
   it('reacts to HSL change correctly', async () => {
     const user = userEvent.setup();
     render(<Colors />);
@@ -28,11 +30,13 @@ describe('Colors', () => {
     const hsl = screen.getByLabelText('HSL');
     const rgb = screen.getByLabelText('RGB');
 
-    await user.clear(hex);
-    await user.type(hsl, '0, 0, 0');
-    expect(hex.innerHTML).toBe('#000000');
-    expect(rgb.innerHTML).toBe('0, 0, 0');
+    await user.clear(hsl);
+    await user.type(hsl, '39, 24, 52');
+    expect(hsl).toHaveValue('39, 24, 52');
+    expect(hex).toHaveValue('A28D67');
+    expect(rgb).toHaveValue('162, 141, 103');
   });
+
   it('reacts to RGB change correctly', async () => {
     const user = userEvent.setup();
     render(<Colors />);
@@ -41,9 +45,10 @@ describe('Colors', () => {
     const hsl = screen.getByLabelText('HSL');
     const rgb = screen.getByLabelText('RGB');
 
-    await user.clear(hex);
-    await user.type(rgb, '0, 0, 0');
-    expect(hsl.innerHTML).toBe('0, 0, 0');
-    expect(hex.innerHTML).toBe('#000000');
+    await user.clear(rgb);
+    await user.type(rgb, '201, 57, 123');
+    expect(rgb).toHaveValue('201, 57, 123');
+    expect(hsl).toHaveValue('333, 55, 51');
+    expect(hex).toHaveValue('C9397B');
   });
 });
--- pages/colors.tsx
+++ pages/colors.tsx
@@ -1,19 +1,24 @@
-import { Container, TextField, Typography } from '@mui/material';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import ContentPasteGoIcon from '@mui/icons-material/ContentPasteGo';
+import { TextField, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import * as convert from 'colors-convert';
 import { HEX, HSL, RGB } from 'colors-convert/dist/cjs/lib/types/types';
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { HexColorPicker } from 'react-colorful';
 
 import Heading from '../components/Heading';
 import Layout from '../components/Layout';
+import Toast, { ToastProps } from '../components/Toast';
 import useLocalState from '../hooks/useLocalState';
+import useSupportsClipboardRead from '../hooks/useSupportsClipboardRead';
 
 function serializeColor(value: HEX | HSL | RGB): string {
   if (typeof value === 'object') {
     return Object.values(value).join(', ');
   }
-  return value;
+  return value.replace(/^#/, '');
 }
 
 function stringToNumArray(value: string): number[] | string {
@@ -29,21 +34,39 @@ function stringToNumArray(value: string): number[] | string {
 }
 
 export default function Colors() {
+  const supportsClipboardRead = useSupportsClipboardRead();
+  const [toastOpen, setToastOpen] = useState<boolean>(false);
+  const [toastMessage, setToastMessage] = useState<string>('');
+  const [toastSeverity, setToastSeverity] =
+    useState<ToastProps['severity']>('success');
+
   const [pickerColor, setPickerColor] = useLocalState<string>({
     key: 'colorPicker_pickerColor',
     defaultValue: '#000000',
   });
   const [hexColor, setHexColor] = useLocalState<string>({
     key: 'colorPicker_hexColor',
-    defaultValue: '#000000',
+    defaultValue: '000000',
+  });
+  const [hexErr, setHexErr] = useLocalState<boolean>({
+    key: 'colorPicker_hexErr',
+    defaultValue: false,
   });
   const [hslColor, setHslColor] = useLocalState<string>({
     key: 'colorPicker_hslColor',
     defaultValue: '0, 0, 0',
   });
+  const [hslErr, setHslErr] = useLocalState<boolean>({
+    key: 'colorPicker_hslErr',
+    defaultValue: false,
+  });
   const [rgbColor, setRGBColor] = useLocalState<string>({
     key: 'colorPicker_rgbColor',
     defaultValue: '0, 0, 0',
+  });
+  const [rgbErr, setRgbErr] = useLocalState<boolean>({
+    key: 'colorPicker_rgbErr',
+    defaultValue: false,
   });
   const [err, setErr] = useLocalState<string>({
     key: 'colorPicker_err',
@@ -51,11 +74,15 @@ export default function Colors() {
   });
 
   function HandleHexChange(hex: string) {
-    setHexColor(hex);
-    if (/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(hex)) {
-      setHslColor(serializeColor(convert.hexToHsl(hex)));
-      setRGBColor(serializeColor(convert.hexToRgb(hex)));
-      setPickerColor(hex);
+    setHexColor(serializeColor(hex));
+    const sanitized = hex.replace(/^#/, '');
+    if (/^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(hex)) {
+      setHslColor(serializeColor(convert.hexToHsl(`#${sanitized}`)));
+      setRGBColor(serializeColor(convert.hexToRgb(`#${sanitized}`)));
+      setPickerColor(`#${sanitized}`);
+    } else {
+      setErr('Invalid Hex Color');
+      setHexErr(true);
     }
   }
 
@@ -74,6 +101,8 @@ export default function Colors() {
       setHslColor(serializeColor(convert.rgbToHsl(rgb)));
     } else {
       setRGBColor(value);
+      setErr('Invalid RGB Color');
+      setRgbErr(true);
     }
   }
 
@@ -81,7 +110,7 @@ export default function Colors() {
     const valArr = stringToNumArray(value);
     if (Array.isArray(valArr)) {
       const hsl = {
-        h: valArr[0] <= 100 ? valArr[0] : 100,
+        h: valArr[0] <= 359 ? valArr[0] : 359,
         s: valArr[1] <= 100 ? valArr[1] : 100,
         l: valArr[2] <= 100 ? valArr[2] : 100,
       };
@@ -92,6 +121,8 @@ export default function Colors() {
       setRGBColor(serializeColor(convert.hslToRgb(hsl)));
     } else {
       setHslColor(value);
+      setErr('Invalid HSL Color');
+      setHslErr(true);
     }
   }
 
@@ -102,6 +133,9 @@ export default function Colors() {
   ) => {
     try {
       setErr('');
+      setHexErr(false);
+      setRgbErr(false);
+      setHslErr(false);
       const { name, value } = eventData.target;
       switch (name) {
         case 'hex':
@@ -126,13 +160,16 @@ export default function Colors() {
   return (
     <Layout>
       <Heading>Colors</Heading>
-      <Container>
-        <Box
-          display='flex'
-          flexDirection='column'
-          alignItems='center'
-          gap='2rem'
-        >
+      <Box
+        width='1000px'
+        maxWidth='100%'
+        display='flex'
+        flexDirection={{ xs: 'column', md: 'row' }}
+        justifyContent='center'
+        alignItems={{ xs: 'center', md: 'flex-start' }}
+        gap='4rem'
+      >
+        <Box>
           <HexColorPicker
             color={pickerColor}
             onChange={(color) => {
@@ -141,45 +178,208 @@ export default function Colors() {
               });
             }}
           />
+        </Box>
+        <Box
+          display='flex'
+          flexDirection='column'
+          justifyContent='stretch'
+          gap={4}
+        >
           <Box
             display='flex'
             flexDirection='column'
-            justifyContent='stretch'
-            gap={6}
-            width={1000}
-            maxWidth='100%'
+            justifyContent='center'
+            alignItems='flex-end'
+            gap={1}
           >
             <TextField
               label='HEX'
               name='hex'
+              error={hexErr}
               value={hexColor}
               InputLabelProps={{
                 shrink: true,
               }}
               onChange={updateColors}
             />
+            <Box
+              display='flex'
+              flexWrap='wrap'
+              gap={1}
+            >
+              <Button
+                startIcon={<ContentCopyIcon />}
+                disabled={!hexColor}
+                onClick={() => {
+                  navigator.clipboard.writeText(hexColor || '').then(
+                    () => {
+                      setToastMessage('Copied to clipboard');
+                      setToastSeverity('success');
+                      setToastOpen(true);
+                    },
+                    () => {
+                      setToastMessage('Failed to copy to clipboard');
+                      setToastSeverity('error');
+                      setToastOpen(true);
+                    },
+                  );
+                }}
+              >
+                Copy
+              </Button>
+              {!!supportsClipboardRead && (
+                <Button
+                  startIcon={<ContentPasteGoIcon />}
+                  onClick={async () => {
+                    const text = await navigator.clipboard.readText();
+                    if (text) {
+                      setHexColor(text);
+                      updateColors({
+                        target: { name: 'hex', value: text },
+                      });
+                    }
+                  }}
+                >
+                  Paste
+                </Button>
+              )}
+            </Box>
+          </Box>
+          <Box
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='flex-end'
+            gap={1}
+          >
             <TextField
               label='HSL'
               name='hsl'
+              error={hslErr}
               value={hslColor}
               InputLabelProps={{
                 shrink: true,
               }}
               onChange={updateColors}
             />
+            <Box
+              display='flex'
+              flexWrap='wrap'
+              gap={1}
+            >
+              <Button
+                startIcon={<ContentCopyIcon />}
+                disabled={!hslColor}
+                onClick={() => {
+                  navigator.clipboard.writeText(hslColor || '').then(
+                    () => {
+                      setToastMessage('Copied to clipboard');
+                      setToastSeverity('success');
+                      setToastOpen(true);
+                    },
+                    () => {
+                      setToastMessage('Failed to copy to clipboard');
+                      setToastSeverity('error');
+                      setToastOpen(true);
+                    },
+                  );
+                }}
+              >
+                Copy
+              </Button>
+              {!!supportsClipboardRead && (
+                <Button
+                  startIcon={<ContentPasteGoIcon />}
+                  onClick={async () => {
+                    const text = await navigator.clipboard.readText();
+                    if (text) {
+                      setHslColor(text);
+                      updateColors({
+                        target: { name: 'hsl', value: text },
+                      });
+                    }
+                  }}
+                >
+                  Paste
+                </Button>
+              )}
+            </Box>
+          </Box>
+          <Box
+            display='flex'
+            flexDirection='column'
+            justifyContent='center'
+            alignItems='flex-end'
+            gap={1}
+          >
             <TextField
               label='RGB'
               name='rgb'
+              error={rgbErr}
               value={rgbColor}
               onChange={updateColors}
               InputLabelProps={{
                 shrink: true,
               }}
             />
-            <Typography variant='body1'>{err}</Typography>
+            <Box
+              display='flex'
+              flexWrap='wrap'
+              gap={1}
+            >
+              <Button
+                startIcon={<ContentCopyIcon />}
+                disabled={!rgbColor}
+                onClick={() => {
+                  navigator.clipboard.writeText(rgbColor || '').then(
+                    () => {
+                      setToastMessage('Copied to clipboard');
+                      setToastSeverity('success');
+                      setToastOpen(true);
+                    },
+                    () => {
+                      setToastMessage('Failed to copy to clipboard');
+                      setToastSeverity('error');
+                      setToastOpen(true);
+                    },
+                  );
+                }}
+              >
+                Copy
+              </Button>
+              {!!supportsClipboardRead && (
+                <Button
+                  startIcon={<ContentPasteGoIcon />}
+                  onClick={async () => {
+                    const text = await navigator.clipboard.readText();
+                    if (text) {
+                      setRGBColor(text);
+                      updateColors({
+                        target: { name: 'rgb', value: text },
+                      });
+                    }
+                  }}
+                >
+                  Paste
+                </Button>
+              )}
+            </Box>
           </Box>
+          <Typography
+            variant='body1'
+            color='error'
+            minHeight='26px'
+          >
+            {err}
+          </Typography>
         </Box>
-      </Container>
+      </Box>
+      <Toast
+        open={toastOpen}
+        message={toastMessage}
+        severity={toastSeverity}
+        onClose={() => setToastOpen(false)}
+      />
     </Layout>
   );
 }
